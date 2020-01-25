6fd94f517694819ffef48a4f703a2fe2df8ed369
SeisoLapis <perridot@mail.ru>
2020-01-25 14:18:48 +0300

    Kirill =/

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 d = True
 a = ""
 
@@ -11,6 +12,12 @@ def main():
         y4 = 0
         wei = 15
         speed = 5
+        sume = 0
+        avalible = False
+        avalible1 = False
+        avalible2 = False
+        avalible3 = False
+        avalible4 = False
         invisible = True
         invisible1 = True
         invisible2 = True
@@ -85,33 +92,79 @@ def main():
             if invisible4:
                 pygame.draw.circle(x1, (255, 255, 0), (512, y4), (wei))
 
-            if y1 >= 440:
-                if y1 <= 470:
+            sum1 = str(sume)
+            c1 = pygame.font.Font(None, 36)
+            text1 = c1.render(sum1, 1, (180, 0, 0))
+
+            x1.blit(text1, (10, 50))
+
+            # if avalible == False:
+            #    if keys[pygame.K_q] or keys[pygame.K_w] or keys[pygame.K_e] or keys[pygame.K_r]:
+            #        sume -= 1
+
+            if avalible1 == False:
+                if keys[pygame.K_q]:
+                    sume -= 1
+
+            if avalible2 == False:
+                if keys[pygame.K_w]:
+                    sume -= 1
+
+            if avalible3 == False:
+                if keys[pygame.K_e]:
+                    sume -= 1
+
+            if avalible4 == False:
+                if keys[pygame.K_r]:
+                    sume -= 1
+
+            if y1 >= 430:
+                if y1 <= 480:
+                    avalible1 = True
                     if keys[pygame.K_q]:
                         invisible1 = False
+                        sume += 1
                         y1 = 0
                         invisible1 = True
 
-            if y2 >= 440:
-                if y2 <= 470:
+            if y1 < 430 or y1 > 480:
+                avalible1 = False
+
+            if y2 >= 430:
+                if y2 <= 480:
+                    avalible2 = True
                     if keys[pygame.K_w]:
                         invisible2 = False
+                        sume += 1
                         y2 = 0
                         invisible2 = True
 
-            if y3 >= 440:
-                if y3 <= 470:
+            if y2 < 430 or y2 > 480:
+                avalible2 = False
+
+            if y3 >= 430:
+                if y3 <= 480:
+                    avalible3 = True
                     if keys[pygame.K_e]:
+                        sume += 1
                         invisible3 = False
                         y3 = 0
                         invisible3 = True
 
-            if y4 >= 440:
-                if y4 <= 470:
+            if y3 < 430 or y3 > 480:
+                avalible3 = False
+
+            if y4 >= 430:
+                if y4 <= 480:
+                    avalible4 = True
                     if keys[pygame.K_r]:
+                        sume += 1
                         invisible4 = False
                         y4 = 0
                         invisible4 = True
+
+            if y4 < 430 or y4 > 480:
+                avalible4 = False
 
             pygame.display.flip()
 
