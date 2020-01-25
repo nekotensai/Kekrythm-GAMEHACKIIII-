ee908e242f928440577415050e8e99ecc4972830
jaratrozky <jaratrozky@mail.ru>
2020-01-25 23:26:48 +0300

    АНИМЕ ГОВНО

--- jaratest.py
+++ jaratest.py
@@ -1,207 +1,101 @@
 import time
-
 import pygame
 
-d = True
-a = ""
-v = [(1,1000),(2,100000)]
+pygame.init()
+pygame.font.init() 
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+v = [('G',28),('B',50),('R',100)]
+color = {'B':512//4,'G':512*2//4,'Y':512*3//4,'R':512*4//4}
+fps = 60
+clock = pygame.time.Clock()
 
+
+class slider():
+    def __init__(self, x, length, screen, velocity):
+        self.r = 15
+        self.b = 1
+        self.length = length * velocity * fps
+        self.x = x
+        self.y = 0 - self.r - self.length
+        self.screen = screen
+        self.velocity = velocity
+        self.color = (255, 255, 255)
+
+    def move():
+        self.y += self.velocity
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y + self.length), self.r)
+        pygame.draw.rect(self.screen, self.color, (self.x - self.r, self.y , self.r * 2, self.length), self.b)
+
+    def move(self):
+        self.y += self.velocity
+
+    def __del__(self):
+        self.y = 512
+        self.color = (0,0,0)
+        self.b = 0
+
+
+class note():
+    def __init__(self, x, screen, velocity):
+        self.r = 15
+        self.x = x
+        self.y = 0 - self.r
+        self.screen = screen
+        self.velocity = velocity
+        self.color = (255, 255, 255)
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
+
+    def move(self):
+        self.y += self.velocity
+
+    def __del__(self):
+        self.y = 512
+        self.color = (0,0,0)
+        self.r = 0
 
 
 def main():
-    while d:
-        kek=time.time()
-        y = 0
-        y1 = 0
-        y2 = 0
-        y3 = 0
-        y4 = 0
-        y5 = 0
-        wei = 15
-        speed = 5
-        sume = 0
-        avalible = False
-        avalible1 = False
-        avalible2 = False
-        avalible3 = False
-        avalible4 = False
-        invisible = True
-        invisible1 = True
-        invisible2 = True
-        invisible3 = True
-        invisible4 = True
-        blue = False
-        green = False
-        red = False
-        yellow = False
+    temp = 0
+    mas = []
+    points = 0
+    timer = 0
 
-        a = True
-        pygame.init()
-        x1 = pygame.display.set_mode((640, 512))
+    while True:
+        screen = pygame.display.set_mode((640, 512))
+        if temp == 0:
+            a = slider(100, 1, screen, 3)
 
+        draw_field(screen)
+
+        a.draw()
+        a.move()
+
+        pygame.display.flip()
+
+        clock.tick(fps)
+        temp += 1
+
+
+def draw_field(screen):
         pygame.display.set_caption("KekRythm")
-
-        run = True
-        a1 = 0
-        while run:
-            pygame.time.delay(50)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    exit()
-            keys = pygame.key.get_pressed()
-
-            x1.fill((0, 0, 0))
-            pygame.draw.line(x1, (255, 255, 255), (0, 455), (640, 455), 1)
-            pygame.draw.line(x1, (0, 0, 255), (128, 0), (128, 512), 1)
-            pygame.draw.line(x1, (0, 255, 0), (256, 0), (256, 512), 1)
-            pygame.draw.line(x1, (255, 0, 0), (384, 0), (384, 512), 1)
-            pygame.draw.line(x1, (255, 255, 0), (512, 0), (512, 512), 1)
-            pygame.draw.circle(x1, (255, 255, 255), (128, 455), 17)
-            pygame.draw.circle(x1, (0, 0, 0), (128, 455), 15)
-            pygame.draw.circle(x1, (255, 255, 255), (256, 455), 17)
-            pygame.draw.circle(x1, (0, 0, 0), (256, 455), 15)
-            pygame.draw.circle(x1, (255, 255, 255), (384, 455), 17)
-            pygame.draw.circle(x1, (0, 0, 0), (384, 455), 15)
-            pygame.draw.circle(x1, (255, 255, 255), (512, 455), 17)
-            pygame.draw.circle(x1, (0, 0, 0), (512, 455), 15)
-
-            if y1 > 530:
-                invisible1 = False
-                y1 = 0
-                invisible1 = True
-            if invisible1 and blue:
-                pygame.draw.circle(x1, (0, 0, 255), (128, y1), (wei))
-
-            if y2 > 530:
-                invisible2 = False
-                y2 = 0
-                invisible2 = True
-            if invisible2 and green:
-                pygame.draw.circle(x1, (0, 255, 0), (256, y2), (wei))
-
-            if y3 > 530:
-                invisible3 = False
-                y3 = 0
-                invisible3 = True
-            if invisible3 and red:
-                pygame.draw.circle(x1, (255, 0, 0), (384, y3), (wei))
-
-            if y4 > 530:
-                invisible4 = False
-                y4 = 0
-                invisible4 = True
-            if invisible4 and yellow:
-                pygame.draw.circle(x1, (255, 255, 0), (512, y4), (wei))
-
-            sum1 = str(sume)
-            c1 = pygame.font.Font(None, 36)
-            text1 = c1.render(sum1, 1, (180, 0, 0))
-
-            x1.blit(text1, (10, 50))
-
-            #  if avalible == False:
-            #    if keys[pygame.K_q] or keys[pygame.K_w] or keys[pygame.K_e] or keys[pygame.K_r]:
-            #        sume -= 1
-
-            if avalible1 == False:
-                if keys[pygame.K_q]:
-                    sume -= 1
-
-            if avalible2 == False:
-                if keys[pygame.K_w]:
-                    sume -= 1
-
-            if avalible3 == False:
-                if keys[pygame.K_e]:
-                    sume -= 1
-
-            if avalible4 == False:
-                if keys[pygame.K_r]:
-                    sume -= 1
-
-            if y1 >= 430:
-                if y1 <= 480:
-                    avalible1 = True
-                    if keys[pygame.K_q]:
-                        # invisible1 = False
-                        sume += 1
-                        y1 = 0
-                        invisible1 = False
-                        blue = False
-
-            if y1 < 430 or y1 > 480:
-                avalible1 = False
-
-            if y2 >= 430:
-                if y2 <= 480:
-                    avalible2 = True
-                    if keys[pygame.K_w]:
-                        # invisible2 = False
-                        sume += 1
-                        y2 = 0
-                        invisible2 = False
-                        green = False
-
-            if y2 < 430 or y2 > 480:
-                avalible2 = False
-
-            if y3 >= 430:
-                if y3 <= 480:
-                    avalible3 = True
-                    if keys[pygame.K_e]:
-                        sume += 1
-                        # invisible3 = False
-                        y3 = 0
-                        invisible3 = False
-                        red = False
-
-            if y3 < 430 or y3 > 480:
-                avalible3 = False
-
-            if y4 >= 430:
-                if y4 <= 480:
-                    avalible4 = True
-                    if keys[pygame.K_r]:
-                        sume += 1
-                        # invisible4 = False
-                        y4 = 0
-                        invisible4 = False
-                        yellow = False
-            if y4 < 430 or y4 > 480:
-                avalible4 = False
-
-            pygame.display.flip()
-
-            if d and invisible1 == True:
-                y1 += speed
-            if d and invisible2 == True:
-                y2 += speed
-            if d and invisible3 == True:
-                y3 += speed
-            if d and invisible4 == True:
-                y4 += speed
-
-            while len(v) != 0:
-                mute = v.pop(0)
-                if (time.time()-kek <mute[1]):
-                    if mute[0] == 1:
-                        blue = True
-                        invisible1 = True
-                    elif mute[0] == 2:
-                        green = True
-                        invisible2 = True
-                    elif mute[0] == 3:
-                        red = True
-                        invisible3 = True
-                    elif mute[0] == 4:
-                        yellow = True
-                        invisible4 = True
-                    elif mute == "_":
-                        time.sleep(1)
-
-    return 0
+        pygame.draw.line(screen, (255, 255, 255), (0, 455), (640, 455), 1)
+        pygame.draw.line(screen, (0, 0, 255), (128, 0), (128, 512), 1)
+        pygame.draw.line(screen, (0, 255, 0), (256, 0), (256, 512), 1)
+        pygame.draw.line(screen, (255, 255, 0), (384, 0), (384, 512), 1)
+        pygame.draw.line(screen, (255, 0, 0), (512, 0), (512, 512), 1)
+        pygame.draw.circle(screen, (255, 255, 255), (128, 455), 17)
+        pygame.draw.circle(screen, (0, 0, 0), (128, 455), 15)
+        pygame.draw.circle(screen, (255, 255, 255), (256, 455), 17)
+        pygame.draw.circle(screen, (0, 0, 0), (256, 455), 15)
+        pygame.draw.circle(screen, (255, 255, 255), (384, 455), 17)
+        pygame.draw.circle(screen, (0, 0, 0), (384, 455), 15)
+        pygame.draw.circle(screen, (255, 255, 255), (512, 455), 17)
+        pygame.draw.circle(screen, (0, 0, 0), (512, 455), 15)
 
 
 if __name__ == "__main__":
--- kekovo.py
+++ kekovo.py
@@ -2,8 +2,41 @@ import time
 import pygame
 
 pygame.init()
-v = [('Y',28),('Y',50),('Y',100)]
-color = {'B':512//4,'G':512*2//4,'Y':512*3//4,'R':512*4//4,}
+pygame.font.init() 
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+v = [('N','G',28),('N','B',50),('S','R',100,60),('N','B',130)]
+color = {'B':512//4,'G':512*2//4,'Y':512*3//4,'R':512*4//4}
+fps = 60
+clock = pygame.time.Clock()
+
+
+class slider():
+    def __init__(self, x, screen, velocity, length):
+        self.r = 15
+        self.b = 1
+        self.length = length * velocity
+        self.x = x
+        self.y = 0 - self.r - self.length
+        self.screen = screen
+        self.velocity = velocity
+        self.color = (255, 255, 255)
+
+    def move():
+        self.y += self.velocity
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y + self.length), self.r)
+        pygame.draw.rect(self.screen, self.color, (self.x - self.r, self.y , self.r * 2, self.length), self.b)
+
+    def move(self):
+        self.y += self.velocity
+
+    def __del__(self):
+        self.y = 512
+        self.color = (0,0,0)
+        self.b = 0
+
 
 class note():
     def __init__(self, x, screen, velocity):
@@ -12,20 +45,102 @@ class note():
         self.y = 0 - self.r
         self.screen = screen
         self.velocity = velocity
+        self.color = (255, 255, 255)
 
     def draw(self):
-        pygame.draw.circle(self.screen, (255,255,255), (self.x, self.y), self.r)
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
+
     def move(self):
         self.y += self.velocity
+
     def __del__(self):
-        pass
+        self.y = 512
+        self.color = (0,0,0)
+        self.r = 0
+
 
 def main():
     temp = 0
-    mas = []
+    notes = []
+    sliders = []
+    points = 0
+    timer = 0
     while True:
         screen = pygame.display.set_mode((640, 512))
 
+        draw_field(screen)
+
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                exit()
+            elif i.type == pygame.KEYUP:
+                # flag = False
+                if i.key == pygame.K_q:
+                    xtemp = 512//4
+                elif i.key == pygame.K_w:
+                    xtemp = 512*2//4
+                elif i.key == pygame.K_e:
+                    xtemp = 512*3//4
+                elif i.key == pygame.K_r:
+                    xtemp = 512*4//4
+                elif i.key == pygame.K_u:
+                    exit()
+                else:
+                    xtemp = -100
+
+                for i in notes:
+                    if i.y < 465 and i.y > 445 and i.x == xtemp:
+                        points += 1
+                        print('gud')
+                        i.__del__()
+
+                for i in sliders:
+                    i[1] += 1
+                    if i[0].y < 465 and i[0].y + i[0].length > 445 and i[0].x == xtemp:
+                        if i[1] >= 7:
+                            i[1] = 0
+                            points += 1
+
+
+                        # flag = True
+                # if not flag:
+                #     points -=1
+
+        if v[0][2] == temp:
+            if v[0][0] == 'N':
+                notes.append(note(color[v[0][1]], screen, 3))
+
+            elif v[0][0] == 'S':
+                sliders.append([slider(color[v[0][1]],screen, 3, v[0][3]),10])
+
+            v.pop(0)
+            if len(v) == 0:
+                v.append((0, 0, -1))
+
+        for i in notes:
+            if i.y > 512:
+                del i
+            else:
+                i.draw()
+                i.move()
+
+        for i in sliders:
+            if i[0].y > 512:
+                del i
+            else:
+                i[0].draw()
+                i[0].move()
+
+        textsurface = myfont.render(str(points), False, (255, 255, 255))
+        screen.blit(textsurface,(50,10))
+
+        pygame.display.flip()
+
+        clock.tick(fps)
+        temp += 1
+
+
+def draw_field(screen):
         pygame.display.set_caption("KekRythm")
         pygame.draw.line(screen, (255, 255, 255), (0, 455), (640, 455), 1)
         pygame.draw.line(screen, (0, 0, 255), (128, 0), (128, 512), 1)
@@ -40,24 +155,6 @@ def main():
         pygame.draw.circle(screen, (0, 0, 0), (384, 455), 15)
         pygame.draw.circle(screen, (255, 255, 255), (512, 455), 17)
         pygame.draw.circle(screen, (0, 0, 0), (512, 455), 15)
-
-
-        if v[0][1] == temp:
-            mas.append(note(color[v[0][0]], screen, 3))
-            v.pop(0)
-            if len(v) == 0:v.append((0,-1))
-
-        for i in mas:
-        	if i.y > 512:
-        		del i
-        	else:
-        		i.draw()
-        		i.move()
-
-        pygame.display.flip()
-
-        pygame.time.delay(20)
-        temp += 1
 
 
 if __name__ == "__main__":
