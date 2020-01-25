b84fad7668349c7f23087bc244ec565369e6c55f
SeisoLapis <perridot@mail.ru>
2020-01-25 11:06:05 +0300

    Kirill =/

--- main.py
+++ main.py
@@ -17,13 +17,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-pygame.init()
-x1 = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption("KickRythm")
-
-run = True
-a1 = 0
-while run:
-    pygame.time.delay(50)
