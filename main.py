ff3c107bdb7031fe0e0e07e707d3e9f76b70c4e1
SeisoLapis <perridot@mail.ru>
2020-01-25 12:00:33 +0300

    Kirill =/

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ a = ""
 
 def main():
     while d:
-        x = 100
-        y = 100
-        wei = 10
-        hei = 10
+        x = 128
+        y = 0
+        wei = 15
         speed = 5
+        g = True
 
         a = True
         pygame.init()
-        x1 = pygame.display.set_mode((500, 500))
+        x1 = pygame.display.set_mode((640, 512))
 
-        pygame.display.set_caption("KickRythm")
+        pygame.display.set_caption("KekRythm")
 
         run = True
         a1 = 0
@@ -25,16 +25,23 @@ def main():
                 if event.type == pygame.QUIT:
                     run = False
                     exit()
-
             keys = pygame.key.get_pressed()
 
             x1.fill((0, 0, 0))
-            if y != 480:
+            pygame.draw.line(x1, (255, 255, 255), (0, 475), (640, 475), 1)
+            pygame.draw.line(x1, (0, 0, 255), (128, 0), (128, 512), 1)
+            pygame.draw.line(x1, (0, 255, 0), (256, 0), (256, 512), 1)
+            pygame.draw.line(x1, (255, 0, 0), (384, 0), (384, 512), 1)
+            pygame.draw.line(x1, (255, 255, 0), (512, 0), (512, 512), 1)
+            if y == 480:
+                g = False
+            if g:
+                pygame.draw.circle(x1, (0, 0, 255), (128, 0), (wei))
                 pygame.draw.circle(x1, (0, 0, 255), (x, y), (wei))
 
             pygame.display.flip()
 
-            if y < 500 - wei - 10:
+            if y < 500 - wei:
                 y += speed
 
     return 0
