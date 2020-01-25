d9f71ddddbf3323e054dc937b641b65268683030
Andryxa0333 <andron_0333@mail.ru>
2020-01-25 14:22:02 +0300

    test_comm

--- ATest.py
+++ ATest.py
@@ -1,3 +1,9 @@
 import pygame
-
 pygame.init()
+screen = pygame.display.set_mode((640, 512))
+pygame.display.set_caption("KekRythm")
+while 1:
+    screen.fill((0, 0, 0))
+    pygame.draw.line()
+    pygame.draw.circle(screen, (0, 255, 0), (155, 20), 10)
+    pygame.display.flip()
