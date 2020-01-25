4fb0df903c14fd80349f131b2150e1e1cb7e8446
jaratrozky <jaratrozky@mail.ru>
2020-01-25 23:50:30 +0300

    РУССКИЕ ВПЕРЁД

--- kekovo.py
+++ kekovo.py
@@ -6,12 +6,13 @@ pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 v = [('N','G',28),('N','B',50),('S','R',100,60),('N','B',130)]
 color = {'B':512//4,'G':512*2//4,'Y':512*3//4,'R':512*4//4}
+colors = {'B':(0,0,255),'G':(0,255,0),'Y':(255,255,0),'R':(255,0,0)}
 fps = 60
 clock = pygame.time.Clock()
 
 
 class slider():
-    def __init__(self, x, screen, velocity, length):
+    def __init__(self, x, screen, velocity, color, length):
         self.r = 15
         self.b = 1
         self.length = length * velocity
@@ -19,7 +20,7 @@ class slider():
         self.y = 0 - self.r - self.length
         self.screen = screen
         self.velocity = velocity
-        self.color = (255, 255, 255)
+        self.color = colors[color]
 
     def move():
         self.y += self.velocity
@@ -27,7 +28,7 @@ class slider():
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
         pygame.draw.circle(self.screen, self.color, (self.x, self.y + self.length), self.r)
-        pygame.draw.rect(self.screen, self.color, (self.x - self.r, self.y , self.r * 2, self.length), self.b)
+        pygame.draw.rect(self.screen, self.color, (self.x - self.r, self.y , self.r * 2, self.length))
 
     def move(self):
         self.y += self.velocity
@@ -39,13 +40,13 @@ class slider():
 
 
 class note():
-    def __init__(self, x, screen, velocity):
+    def __init__(self, x, screen, velocity, color):
         self.r = 15
         self.x = x
         self.y = 0 - self.r
         self.screen = screen
         self.velocity = velocity
-        self.color = (255, 255, 255)
+        self.color = colors[color]
 
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
@@ -91,7 +92,6 @@ def main():
                 for i in notes:
                     if i.y < 465 and i.y > 445 and i.x == xtemp:
                         points += 1
-                        print('gud')
                         i.__del__()
 
                 for i in sliders:
@@ -108,10 +108,10 @@ def main():
 
         if v[0][2] == temp:
             if v[0][0] == 'N':
-                notes.append(note(color[v[0][1]], screen, 3))
+                notes.append(note(color[v[0][1]], screen, 3, v[0][1]))
 
             elif v[0][0] == 'S':
-                sliders.append([slider(color[v[0][1]],screen, 3, v[0][3]),10])
+                sliders.append([slider(color[v[0][1]],screen, 3, v[0][1], v[0][3]),10])
 
             v.pop(0)
             if len(v) == 0:
@@ -142,19 +142,21 @@ def main():
 
 def draw_field(screen):
         pygame.display.set_caption("KekRythm")
-        pygame.draw.line(screen, (255, 255, 255), (0, 455), (640, 455), 1)
+        pygame.draw.rect(screen,(100, 100, 100), (0, 0, 640, 512))
+        # pygame.draw.line(screen, (0, 0, 0), (0, 455), (640, 455), 1)
         pygame.draw.line(screen, (0, 0, 255), (128, 0), (128, 512), 1)
         pygame.draw.line(screen, (0, 255, 0), (256, 0), (256, 512), 1)
         pygame.draw.line(screen, (255, 255, 0), (384, 0), (384, 512), 1)
         pygame.draw.line(screen, (255, 0, 0), (512, 0), (512, 512), 1)
-        pygame.draw.circle(screen, (255, 255, 255), (128, 455), 17)
-        pygame.draw.circle(screen, (0, 0, 0), (128, 455), 15)
-        pygame.draw.circle(screen, (255, 255, 255), (256, 455), 17)
-        pygame.draw.circle(screen, (0, 0, 0), (256, 455), 15)
-        pygame.draw.circle(screen, (255, 255, 255), (384, 455), 17)
-        pygame.draw.circle(screen, (0, 0, 0), (384, 455), 15)
-        pygame.draw.circle(screen, (255, 255, 255), (512, 455), 17)
-        pygame.draw.circle(screen, (0, 0, 0), (512, 455), 15)
+
+        pygame.draw.circle(screen, (255, 255, 255), (128, 455), 15, 1)
+        # pygame.draw.circle(screen, (0, 0, 0), (128, 455), 15)
+        pygame.draw.circle(screen, (255, 255, 255), (256, 455), 15, 1)
+        # pygame.draw.circle(screen, (0, 0, 0), (256, 455), 15)
+        pygame.draw.circle(screen, (255, 255, 255), (384, 455), 15, 1)
+        # pygame.draw.circle(screen, (0, 0, 0), (384, 455), 15)
+        pygame.draw.circle(screen, (255, 255, 255), (512, 455), 15, 1)
+        # pygame.draw.circle(screen, (0, 0, 0), (512, 455), 15)
 
 
 if __name__ == "__main__":
