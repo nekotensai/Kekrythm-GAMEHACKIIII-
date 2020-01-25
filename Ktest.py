75b56205b1fd1ce7f3a5c30a30cf5d3dc1c4ce72
SeisoLapis <perridot@mail.ru>
2020-01-25 11:05:49 +0300

    Kirill =/

---
+++ Ktest.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+x1 = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("KickRythm")
+
+run = True
+a1 = 0
+while run:
+    pygame.time.delay(50)
---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+d = True
+a = ""
+def main():
+    while d:
+        a = True
+        pygame.init()
+        x1 = pygame.display.set_mode((500, 500))
+
+        pygame.display.set_caption("KickRythm")
+
+        run = True
+        a1 = 0
+        while run:
+            pygame.time.delay(50)
+    return 0
+
+if __name__ == "__main__":
+    main()
+
+pygame.init()
+x1 = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("KickRythm")
+
+run = True
+a1 = 0
+while run:
+    pygame.time.delay(50)
